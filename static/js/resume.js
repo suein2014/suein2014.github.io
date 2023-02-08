ce52943004d3877f422d2ff8a1ca2ae5f59a9982
xiaoyan <tianxiaoyan2014@gmail.com>
2023-02-08 11:58:27 -0800

    homework1a for week 4

---
+++ static/js/resume.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+
+
+    var $header_btn = $("header h1"),
+    delaytime = 500,
+    original_btn_styles= $header_btn.css(['color','font-size']);
+
+    $("button").on('click',function(){
+        $header_btn.css({'color':'red', 'font-size':'xx-large'});
+
+        $('figure img').css({'border':'0px solid white'}).animate({borderWidth:"2px"});
+
+        setTimeout(function(){
+            $.each(original_btn_styles, function(key, value){
+               $header_btn.css( key,value )
+            })
+        }, delaytime);
+  });
+
+
+
+});
