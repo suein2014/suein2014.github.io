23b17798a808357ca0bbbfc873f06a11b310c74c
suein_tian <tianxiaoyan2014@gmail.com>
2023-02-14 11:26:25 -0800

    update

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-    var $header_btn = $("header h1"),original_btn_styles;
+    var $header_btn = $("header h1");
     delaytime = 1000,
     original_btn_styles= $header_btn.css(['font-size','color']);
 
