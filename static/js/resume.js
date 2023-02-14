4acccb9a7aa21a9cff7b7eed7767921b301ee6d4
xiaoyan <tianxiaoyan2014@gmail.com>
2023-02-14 10:33:39 -0800

    update js file for animation.

--- static/js/resume.js
+++ static/js/resume.js
@@ -1,22 +1,28 @@
 $(document).ready(function(){
 
-    var $header_btn = $("header h1"),
-    delaytime = 500,
-    original_btn_styles= $header_btn.css(['color','font-size']);
+    var $header_btn = $("header h1"),original_btn_styles;
+    delaytime = 1000,
+    original_btn_styles= $header_btn.css(['font-size','color']);
+
 
     $("button").on('click',function(){
         $header_btn.css({'color':'red'});
-        $header_btn.animate({fontSize: '2em'});
+        $header_btn.animate({fontSize: '+=.5rem'}, delaytime);
 
+        // add border for introduction photo
         $('figure img').css({'border':'0px solid white'}).animate({borderWidth:"2px"});
 
-        setTimeout(function(){
-            $.each(original_btn_styles, function(key, value){
-               $header_btn.css( key,value )
-            })
-        }, delaytime);
-  });
 
+        // back to original font size
+        setTimeout(function(){
+            $header_btn.animate({fontSize: '-=.5rem'},delaytime);
+        }, delaytime)
+
+        // back to original color
+        setTimeout(function(){
+            $header_btn.css({color:original_btn_styles['color']});
+        }, delaytime * 2);
+  });
 
 
 });
