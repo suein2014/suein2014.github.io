4693b5069032925b2912ac01e97a010bc42c76f3
xiaoyan <tianxiaoyan2014@gmail.com>
2023-03-14 20:23:40 -0700

    begin to code with hw3

---
+++ hw3/static/js/hw3.js
@@ -0,0 +1,17 @@
+$(document).ready(function(){
+    $('.ui.menu .item').tab();
+
+//   $('.ui.menu .item').on('click', function() {
+//      $('.ui.menu .item').removeClass('active');
+//      $(this).addClass('active');
+//   });
+
+//    $('.ui.menu .item').on('click', function() {
+//        var tabName = $(this).attr('data-tab'); // 获取菜单项的data-tab属性值
+//        $('.menu .item').removeClass('active'); // 移除所有菜单项的active类
+//        $(this).addClass('active'); // 添加当前菜单项的active类
+//        $('.tab').hide(); // 隐藏所有tab面板
+//        $('.tab[data-tab="' + tabName + '"]').show(); // 显示对应的tab面板
+//    });
+
+});
