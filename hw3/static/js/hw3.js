fc58a3c71069a4457f48934c2f980706a46b30cc
xiaoyan <tianxiaoyan2014@gmail.com>
2023-03-26 16:05:59 -0700

    finish sidebar on mobile

--- hw3/static/js/hw3.js
+++ hw3/static/js/hw3.js
@@ -1,17 +1,9 @@
 $(document).ready(function(){
     $('.ui.menu .item').tab();
 
-//   $('.ui.menu .item').on('click', function() {
-//      $('.ui.menu .item').removeClass('active');
-//      $(this).addClass('active');
-//   });
 
-//    $('.ui.menu .item').on('click', function() {
-//        var tabName = $(this).attr('data-tab'); // 获取菜单项的data-tab属性值
-//        $('.menu .item').removeClass('active'); // 移除所有菜单项的active类
-//        $(this).addClass('active'); // 添加当前菜单项的active类
-//        $('.tab').hide(); // 隐藏所有tab面板
-//        $('.tab[data-tab="' + tabName + '"]').show(); // 显示对应的tab面板
-//    });
 
-});
+    $('.ui.sidebar').sidebar({
+      context: $('.bottom.attached.segment')
+    }).sidebar('attach events', '.dropdown.item');
+})
