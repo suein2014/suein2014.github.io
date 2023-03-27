80ec808b693448da05c9bd7fb78748a02c1098e5
xiaoyan <tianxiaoyan2014@gmail.com>
2023-03-26 20:29:43 -0700

    fix some code style

--- hw3/static/js/hw3.js
+++ hw3/static/js/hw3.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
     $('.ui.menu .item').tab();
 
-
+     $('.ui.accordion').accordion();
 
     $('.ui.sidebar').sidebar({
       context: $('.bottom.attached.segment')
