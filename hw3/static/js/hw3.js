e119a78863ba94f298d5dc3d5c00923950a044c1
xiaoyan <tianxiaoyan2014@gmail.com>
2023-03-29 13:05:24 -0700

    fix bug: map showed on the left-top corner, to show centered full of the div.

--- hw3/static/js/hw3.js
+++ hw3/static/js/hw3.js
@@ -1,13 +1,11 @@
 
 function show_map_div(){
         // Create a leaflet map object.
-//        var map = L.map('map').setView([37.7749, -122.4194], 13);
         var map = L.map('map').setView([37.7749, -122.4194], 13);
-        map.invalidateSize();
 
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
-          maxZoom: 18
+          maxZoom: 18,
         }).addTo(map);
 
         // when change to the tab: location-page
@@ -21,31 +19,16 @@ function show_map_div(){
               // show position data in the tab title.
               $('a[data-tab="location-page"]').text('Location (' + map.getCenter().toString() + ')');
 
-//               $('a[data-tab="location-page"]').on('shown.bs.tab', function() {
-//  map.invalidateSize();
-//});
               // add a marker, mark user's position
               var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
               marker.bindPopup("You are here!").openPopup();
             });
+            //IMPORTANT: updaet the layout when container size changed
+            map.invalidateSize();
           } else {
             alert("Geolocation is not supported by your browser.");
           }
         });
-
-
-//        var locationpage = document.getElementById("location-page");
-//
-////        document.getElementById("map").addEventListener("click", function () {
-//          locationpage.style.display = "block";
-//            map.invalidateSize(); // Uncomment this line and re-run to get correct behaviour.
-////            map2.fitBounds([[50.5, 5], [52.5, 6]]);
-////        });
-
-//        document.getElementById("hidelocation").addEventListener("click", function () {
-//            locationpage.style.display = "none";
-//        });
-
     }
 
 
@@ -53,8 +36,6 @@ function show_map_div(){
 
 function show_map_mobile(){
         var mobile_map = L.map('m-map').setView([37.7749, -122.4194], 13);
-        mobile_map.invalidateSize();
-
 
         // add a map layer
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
@@ -75,39 +56,12 @@ function show_map_mobile(){
               var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(mobile_map);
               marker.bindPopup("You are here!").openPopup();
             });
+            //IMPORTANT: updaet the layout when container size changed
+            mobile_map.invalidateSize();
           } else {
             alert("Geolocation is not supported by your browser.");
           }
         });
-
-}
-
-
-
-
-
-function show_map_canvas(){
-
-    // Create the map on the canvas element
-    var map = L.map('map', {
-      crs: L.CRS.Simple,
-      minZoom: -2,
-      maxZoom: 2
-    });
-
-    // Create a tile layer with OpenStreetMap tiles
-    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-      attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
-    }).addTo(map);
-
-    // Add a marker at the center of the map
-    var marker = L.marker([0, 0]).addTo(map);
-
-    // Update the marker position when the map is moved
-    map.on('move', function(e) {
-      var center = map.getCenter();
-      marker.setLatLng(center);
-    });
 
 }
 
