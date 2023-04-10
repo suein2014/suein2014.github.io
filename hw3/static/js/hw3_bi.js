46e73ddc53f4e7cca4346da8a5708ed69b85bbff
xiaoyan <tianxiaoyan2014@gmail.com>
2023-04-10 00:32:09 -0700

    完成bootstrap代码。。。没有做html5语义标签

--- hw3/static/js/hw3_bi.js
+++ hw3/static/js/hw3_bi.js
@@ -1,6 +1,9 @@
-     // main function
+
+  // main function
     $(function(){
-        show_map();
+        setTimeout(function() { //fix the issue of map not filling the container
+             show_map();
+         }, 100);
 
 
     });
@@ -9,46 +12,30 @@
     //function to show the map
     function show_map(){
         var map = L.map('mapid').setView([37.7749, -122.4194], 13);
-
-
+        map.invalidateSize();
         L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
           attribution: 'Map data © <a href="https://openstreetmap.org">OpenStreetMap</a> contributors',
           maxZoom: 18,
         }).addTo(map);
 
-        // when change to the tab: location-page
-        $('a[data-tab="location-page"]').on('click', function() {
-
-
-
-
-     // 绑定点击事件，初始化地图并显示
-//      $('a[data-toggle="tab"]').on('shown.bs.tab', function (e) {
-//        if (e.target.hash === './location.html') {
-
-
+        $('a[data-bs-toggle="tab"]').on('shown.bs.tab', function (e) {
+        // 如果当前页签是“Home”页签
+        if ($(e.target).attr('aria-controls') == 'location') {
           // get the current position for user
           if ("geolocation" in navigator) {
             navigator.geolocation.getCurrentPosition(function(position) {
               // set it as map center
               map.setView([position.coords.latitude, position.coords.longitude], 13);
-
-              // show position data in the title.
-//              $('a[data-tab="location-page"]').text('Location (' + map.getCenter().toString() + ')');
-//              $('#mapid').prev('h2').text('Location Page - ' + map.getCenter().toString());
-
+              map.invalidateSize();
+              $('#mapid').prev('h2').text('Location - ' + map.getCenter().toString());
               // add a marker, mark user's position
               var marker = L.marker([position.coords.latitude, position.coords.longitude]).addTo(map);
-
-
               marker.bindPopup("You are here!").openPopup();
             });
             //IMPORTANT: update the layout when container size changed
-            map.invalidateSize();  //for refresh
           } else {
             alert("Geolocation is not supported by your browser.");
-          }
-//        };
-//      });
+          };
+        };
         });
     }
